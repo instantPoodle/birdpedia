6ad04c29a418cf828d1e70ac169e89f049419966
SlushFuzzy <slushfuzzy@users.noreply.github.com>
2018-03-31 22:45:13 +0100

    Pick up from Adding the bird REST API handlers

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ import (
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", handler).Methods("GET")
-	return r
+    staticFileDirectory := http.Dir("./assets/")
+    staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+    r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+    return r
 }
 
 func main() {
